322de09a8e840054a1724a012d914200c5c5d1c1
phongnq424 <ngcthanh1567@gmail.com>
2025-01-25 09:47:31 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const text = "WELCOME TO PHONG PROFILE";
+const header = document.querySelector('.header');
+
+let index = 0;
+function typeEffect() {
+  header.textContent = text.slice(0, index); // Cắt từng phần của chữ
+  index++;
+
+  if (index > text.length) {
+    index = 1; // Reset lại từ đầu
+  }
+
+  setTimeout(typeEffect, 120); 
+}
+
+typeEffect();
